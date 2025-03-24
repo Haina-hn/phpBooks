96fe8e86ef1105d296ce0ae1946709800bb58a98
GriffithMoss <griffithmoss@hotmail.com>
2025-03-24 13:58:04 +0900

    16-22

--- nyuka_kakunin.php
+++ nyuka_kakunin.php
@@ -77,17 +77,21 @@ foreach($_POST['books'] as $booksId/* ⑪の処理を書く */){
 	}
 
 	//⑯「getByid」関数を呼び出し、変数に戻り値を入れる。その際引数に⑪の処理で取得した値と⑧のDBの接続情報を渡す。
-
+	$book = getByid($booksId,$dbh);
 	//⑰ ⑯で取得した書籍の情報の「stock」と、⑩の変数を元にPOSTの「stock」から値を取り出し、足した値を変数に保存する。
-
+	$newStock = $book['stock'] + $_POST['stock'][$book_count];
 	//⑱ ⑰の値が100を超えているか判定する。超えていた場合はif文の中に入る。
-	if(/* ⑱の処理を行う */){
+	if($newStock > 100/* ⑱の処理を書く */){
 		//⑲SESSIONの「error」に「最大在庫数を超える数は入力できません」と設定する。
+		$_SESSION['error'] = '最大在庫数を超える数は入力できません';
 		//⑳「include」を使用して「nyuka.php」を呼び出す。
+		include 'nyuka.php';
 		//㉑「exit」関数で処理を終了する。
+		exit();
 	}
 	
 	//㉒ ⑩で宣言した変数をインクリメントで値を1増やす。
+	$book_count++;
 }
 
 /*
