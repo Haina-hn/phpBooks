80f2a592b4371c1726e0400420490e267d25421f
GriffithMoss <griffithmoss@hotmail.com>
2025-03-24 13:49:40 +0900

    １２番直し

--- nyuka_kakunin.php
+++ nyuka_kakunin.php
@@ -67,7 +67,7 @@ foreach($_POST['books'] as $booksId/* ⑪の処理を書く */){
 	 * 半角数字以外の文字が設定されていないかを「is_numeric」関数を使用して確認する。
 	 * 半角数字以外の文字が入っていた場合はif文の中に入る。
 	 */
-	if (!is_numeric($_POST['stock'][$counter])/* ⑫の処理を書く */) {
+	if (!is_numeric($_POST['stock'][$book_count])/* ⑫の処理を書く */) {
 		//⑬SESSIONの「error」に「数値以外が入力されています」と設定する。
 		$_SESSION['error'] = '数値以外が入力されています';
 		//⑭「include」を使用して「nyuka.php」を呼び出す。
